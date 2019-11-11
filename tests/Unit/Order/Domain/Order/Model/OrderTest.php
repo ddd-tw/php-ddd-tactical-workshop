6a1d8f7ff879ef02bcba22d7428f840eed560c8d
Eric G. Huang <eric.g.yuan@gmail.com>
2019-11-11 11:43:30 +0800

    建立第一個空的 test case

---
+++ tests/Unit/Order/Domain/Order/Model/OrderTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Unit\Order\Domain\Order\Model;
+
+use Tests\TestCase;
+
+class OrderTest extends TestCase
+{
+    public function testCreateOrder_StatusMustBeCreated(): void
+    {
+        //todo: 這裡建立你的訂單建立測試，確認訂單建立後的狀態是否正確
+    }
+}
